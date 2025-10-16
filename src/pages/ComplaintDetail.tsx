d8f40cbc387e9457f5b652ef5d932a95589f28fe
CordeliusVox <cordeliusvox@gmail.com>
2025-10-16 15:17:50 +0300

    Update ComplaintDetail.tsx

--- src/pages/ComplaintDetail.tsx
+++ src/pages/ComplaintDetail.tsx
@@ -1,3 +1,4 @@
+// ComplaintDetail.tsx
 import { useEffect, useMemo, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
@@ -137,30 +138,312 @@ const ComplaintDetail = () => {
     }
   };
 
+  // ----------------------------
+  // No-deps DOCX HTML template filling (uses user's provided HTML)
+  // ----------------------------
+  const escapeHtml = (str: string) =>
+    String(str ?? "")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+
+  const nl2br = (s: string) => escapeHtml(s).replace(/\r\n|\n\r|\n|\r/g, "<br>");
+
   const handleDownloadComplaint = () => {
-    const content = `
-דוח פנייה #${complaint.id}
-============================
+    if (!complaint) return;
 
-כותרת: ${complaint.title}
-קטגוריה: ${complaint.category}
-תאריך הגשה: ${complaint.date}
-מגיש: ${complaint.submitter}
-${complaint.submitterEmail ? `אימייל: ${complaint.submitterEmail}` : ''}
-${complaint.submitterPhone ? `טלפון: ${complaint.submitterPhone}` : ''}
-${complaint.assignedTo ? `מטפל: ${complaint.assignedTo}` : ''}
+    // Fill data
+    const openingDate = escapeHtml(complaint.date || "");
+    const feedbackDate = new Date().toLocaleDateString("he-IL");
+    const customerName = escapeHtml(complaint.submitter || "");
+    const unit = escapeHtml(complaint.category || "");
+    const responderName = escapeHtml(username || "מערכת");
+    const complaintDetailsHtml = nl2br(complaint.description || "");
+    // Use the typed response (if present) as the "התייחסות" field; otherwise use assignedTo or empty.
+    const responseHtml = nl2br(response || complaint.assignedTo || "");
 
-תיאור הפנייה:
-${complaint.description}
+    // If you want to show a logo, put its URL here (public folder) e.g. "/logo.png"
+    const logoUrl = ""; // optional
 
-דוח נוצר בתאריך: ${new Date().toLocaleDateString('he-IL')} ${new Date().toLocaleTimeString('he-IL')}
-    `.trim();
+    // The template is your provided HTML — I removed the external image tag (to avoid broken link).
+    // Placeholders: {{openingDate}}, {{feedbackDate}}, {{customerName}}, {{unit}},
+    // {{responderName}}, {{complaintDetails}}, {{responseText}}, {{logoUrl}}
+    const template = `<!doctype html>
+<html>
 
-    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<meta name=Generator content="Microsoft Word 15 (filtered)">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:Tahoma;
+	panose-1:2 11 6 4 3 5 4 4 2 4;}
+@font-face
+	{font-family:Georgia;
+	panose-1:2 4 5 2 5 4 5 2 3 3;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin-top:0in;
+	margin-right:-.05pt;
+	margin-bottom:0in;
+	margin-left:0in;
+	text-align:left;
+	text-indent:-.05pt;
+	direction:rtl;
+	unicode-bidi:embed;
+	font-size:12.0pt;
+	font-family:"Times New Roman",serif;
+	position:relative;
+	top:.5pt;}
+.MsoChpDefault
+	{font-size:10.0pt;}
+ /* Page Definitions */
+ @page WordSection1
+	{size:595.3pt 841.9pt;
+	margin:42.55pt 56.65pt 1.0in 21.3pt;}
+div.WordSection1
+	{page:WordSection1;}
+-->
+</style>
+
+</head>
+
+<body lang=EN-US link=blue vlink=purple style='word-wrap:break-word'>
+
+<div class=WordSection1>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:115%;border:none'><span dir=LTR style='font-size:
+11.0pt;line-height:115%;font-family:"Arial",sans-serif;color:black'>&nbsp;</span></p>
+
+<div align=left>
+
+<table class=4 dir=rtl border=0 cellspacing=0 cellpadding=0 align=left
+ width=240 style='border-collapse:collapse;margin-left:6.75pt;margin-right:
+ 6.75pt'>
+ <tr>
+  <td width=144 valign=top style='width:1.5in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>חיל</span></p>
+  </td>
+  <td width=96 valign=top style='width:1.0in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:
+  right;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>האוויר</span></p>
+  </td>
+ </tr>
+ <tr>
+  <td width=144 valign=top style='width:1.5in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>המכללה   הטכנולוגית</span></p>
+  </td>
+  <td width=96 valign=top style='width:1.0in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:
+  right;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>באר -  שבע</span></p>
+  </td>
+ </tr>
+ <tr>
+  <td width=144 valign=top style='width:1.5in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>טלפון</span></p>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>סימוכין</span></p>
+  </td>
+  <td width=96 valign=top style='width:1.0in;padding:0in 5.4pt 0in 5.4pt'>
+  <p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:
+  right;text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR></span><span
+  dir=LTR></span><span dir=LTR style='color:black'><span dir=LTR></span><span
+  dir=LTR></span>08-9907410/2</span></p>
+  <p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:
+  right;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>תשפ&quot;ו</span></p>
+  </td>
+ </tr>
+ <tr style='height:16.45pt'>
+  <td width=144 valign=top style='width:1.5in;padding:0in 5.4pt 0in 5.4pt;
+  height:16.45pt'>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='color:black'>תאריך                               </span></p>
+  </td>
+  <td width=96 valign=top style='width:1.0in;padding:0in 5.4pt 0in 5.4pt;
+  height:16.45pt'>
+  <p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:
+  right;text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR
+  style='background:yellow'>{{openingDate}}</span></p>
+  </td>
+ </tr>
+</table>
+
+</div>
+
+<p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR style='color:black'><!-- logo removed to avoid broken link; set logoUrl variable to show logo --></span></p>
+
+<p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR
+style='color:black'>&nbsp;</span></p>
+
+<p class=MsoNormal align=center dir=RTL style='margin-right:.05pt;text-align:
+center;text-indent:-.15pt;line-height:150%;border:none'><b><u><span lang=HE
+style='font-size:14.0pt;line-height:150%;font-family:"Calibri",sans-serif;
+color:black'>משוב לתלונת לקוח</span></u></b></p>
+
+<p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+center;text-indent:-.1pt;line-height:150%;border:none'><span lang=HE
+style='font-family:"Calibri",sans-serif;color:#A6A6A6'>תלונת לקוח היא מנוף
+לשיפור מתמיד, להתחדשות, להבטחת איכות, בקרה ואכיפה של תרבות ארגונית</span></p>
+
+<p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+center;text-indent:-.1pt;line-height:150%;border:none'><u><span dir=LTR
+style='font-family:"Calibri",sans-serif;color:black'><span style='text-decoration:
+ none'>&nbsp;</span></span></u></p>
+
+<div align=right>
+
+<table class=3 dir=rtl border=1 cellspacing=0 cellpadding=0 width=549
+ style='margin-left:34.8pt;border-collapse:collapse;border:none'>
+ <tr style='height:18.3pt'>
+  <td width=111 valign=top style='width:82.95pt;border:solid black 1.0pt;
+  background:#D9D9D9;padding:0in 5.4pt 0in 5.4pt;height:18.3pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='font-family:"Calibri",sans-serif;color:black'>תאריך פתיחה התלונה</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.5pt;border:solid black 1.0pt;
+  border-right:none;background:#D9D9D9;padding:0in 5.4pt 0in 5.4pt;height:18.3pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='font-family:"Calibri",sans-serif;color:black'>תאריך משוב התלונה</span></p>
+  </td>
+  <td width=109 valign=top style='width:81.6pt;border:solid black 1.0pt;
+  border-right:none;background:#D9D9D9;padding:0in 5.4pt 0in 5.4pt;height:18.3pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='font-family:"Calibri",sans-serif;color:black'>שם הלקוח</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.45pt;border:solid black 1.0pt;
+  border-right:none;background:#D9D9D9;padding:0in 5.4pt 0in 5.4pt;height:18.3pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:0in;line-height:normal;border:none'><span lang=HE
+  style='font-family:"Calibri",sans-serif;color:black'>יחידה- גף</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.15pt;border:solid black 1.0pt;
+  border-right:none;background:#D9D9D9;padding:0in 5.4pt 0in 5.4pt;height:18.3pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span lang=HE
+  style='font-family:"Calibri",sans-serif;color:black'>שם המשיב ותפקיד</span></p>
+  </td>
+ </tr>
+ <tr style='height:29.0pt'>
+  <td width=111 valign=top style='width:82.95pt;border:solid black 1.0pt;
+  border-top:none;padding:0in 5.4pt 0in 5.4pt;height:29.0pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR
+  style='font-family:"Calibri",sans-serif;color:black'>${openingDate}</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.5pt;border-top:none;border-left:
+  solid black 1.0pt;border-bottom:solid black 1.0pt;border-right:none;
+  padding:0in 5.4pt 0in 5.4pt;height:29.0pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-top:9.0pt;margin-right:
+  0in;margin-bottom:.25in;margin-left:0in;text-align:center;text-indent:-.1pt;
+  line-height:normal;background:white'><span dir=LTR style='font-family:"Calibri",sans-serif;
+  color:#202124'>${feedbackDate}</span></p>
+  </td>
+  <td width=109 valign=top style='width:81.6pt;border-top:none;border-left:
+  solid black 1.0pt;border-bottom:solid black 1.0pt;border-right:none;
+  padding:0in 5.4pt 0in 5.4pt;height:29.0pt'>
+  <p class=MsoNormal dir=RTL style='margin-right:0in;text-align:justify;
+  text-indent:0in;line-height:normal'><span dir=LTR style='font-family:"Calibri",sans-serif'>${customerName}</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.45pt;border-top:none;border-left:
+  solid black 1.0pt;border-bottom:solid black 1.0pt;border-right:none;
+  padding:0in 5.4pt 0in 5.4pt;height:29.0pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR
+  style='font-family:"Calibri",sans-serif;color:black'>${unit}</span></p>
+  </td>
+  <td width=110 valign=top style='width:82.15pt;border-top:none;border-left:
+  solid black 1.0pt;border-bottom:solid black 1.0pt;border-right:none;
+  padding:0in 5.4pt 0in 5.4pt;height:29.0pt'>
+  <p class=MsoNormal align=center dir=RTL style='margin-right:0in;text-align:
+  center;text-indent:-.1pt;line-height:normal;border:none'><span dir=LTR
+  style='font-family:"Calibri",sans-serif;color:black'>${responderName}</span></p>
+  </td>
+ </tr>
+</table>
+
+</div>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><span dir=LTR style='font-family:
+"Calibri",sans-serif;color:black'>&nbsp;</span></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><u><span lang=HE
+style='font-family:"Calibri",sans-serif;color:black'>פרוט התלונה: </span></u></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><span dir=RTL><span lang=HE style='font-family:"Calibri",sans-serif'>${complaintDetailsHtml}</span></span></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><b><u><span dir=LTR
+style='font-family:"Calibri",sans-serif'><span style='text-decoration:none'>&nbsp;</span></span></u></b></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><u><span lang=HE
+style='font-family:"Calibri",sans-serif;color:black'>התייחסות:</span></u></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:-.1pt;line-height:150%;border:none'><span dir=RTL><span lang=HE style='font-family:"Calibri",sans-serif'>${responseHtml}</span></span></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:0in;text-align:right;
+text-indent:0in;line-height:150%;border:none'><span lang=HE
+style='font-family:"Calibri",sans-serif;color:black'>                                                                                                                                         
+בברכה,</span></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:-.05pt;text-align:
+right;text-indent:0in;line-height:150%;border:none'><span lang=HE
+style='font-family:"Calibri",sans-serif;color:black'>                                                                                                                              
+${responderName}</span></p>
+
+<p class=MsoNormal align=left dir=RTL style='margin-right:-.05pt;text-align:
+right;text-indent:0in;line-height:150%;border:none'><span lang=HE
+style='font-family:"Calibri",sans-serif;color:black'>                                                                                                      
+                  מנהל (או תפקיד)</span></p>
+
+</div>
+
+</body>
+
+</html>`.trim();
+
+    // Generate blob as MS Word HTML (.doc)
+    const blob = new Blob([template], { type: "application/msword;charset=utf-8" });
+
+    const fileNameDate = new Date().toLocaleDateString("he-IL").replace(/\//g, "-");
+    const fileName = `complaint_${escapeHtml(complaint.id)}_${fileNameDate}.doc`;
+
+    // Trigger download
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = `complaint_${complaint.id}_${new Date().toLocaleDateString('he-IL').replace(/\//g, '-')}.txt`;
+    link.download = fileName;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -168,7 +451,7 @@ ${complaint.description}
 
     toast({
       title: "הדוח הורד בהצלחה",
-      description: "קובץ הדוח נשמר במחשב שלך"
+      description: `הקובץ ${fileName} נשמר במחשב שלך`,
     });
   };
 
